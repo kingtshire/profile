0cc3c0c1d7d55fb70e8913766a5b39570642d2d5
Tshireletso P. Tshielo <tshielo01@gmail.com>
2024-02-29 22:38:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,51 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+// send email
+const form = document.querySelector('form');
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+function sendEmail() {
+        const bodyMessage = `Full Name: ${fullName.value}<br> 
+        Email: ${email.value}<br> 
+        Phone Number: ${phone.value}<br> 
+        Message: ${message.value}`;
+
+
+        Email.send({
+                SecureToken : "3ce6b354-8395-47c3-8f30-4cf639263519",
+                To : "tshielo01@gmail.com",
+                From : "tshielo01@gmail.com",
+                Subject : subject.value,
+                Body : bodyMessage
+        
+        }).then(
+                message => {
+                        if (message == "OK") {
+                                Swal.fire({
+                                        title: "Success!",
+                                        text: "Message sent successfully!",
+                                        icon: "success"
+                                      });
+                        }
+                }
+        );
+}
+
+// add the checkInputs function one time
+
+form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        sendEmail();
+
+        form.reset();
+        return false;
+});
+
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
